af9582fa7c153b1ddb3a5206a704b9e089cb7e96
masada-tea <64900337+masada-tea@users.noreply.github.com>
2026-02-22 15:31:44 +0900

    cores.ts を更新

--- examples/demo-retroarch-react-2/src/components/from-local/cores.ts
+++ examples/demo-retroarch-react-2/src/components/from-local/cores.ts
@@ -1,5 +1,5 @@
 import { Mutable } from "../../utils"
 
-export const cores = ["fceumm", "genesis_plus_gx"] as const
+export const cores = ["mkxp-z", "fceumm", "genesis_plus_gx"] as const
 
 export type Core = Mutable<(typeof cores)[number]>
