a653a90a274039bdf40600b41eb30a1e46f7687b
masada-tea <64900337+masada-tea@users.noreply.github.com>
2026-02-22 14:03:22 +0900

    vite.config.ghpages.ts を更新

--- examples/demo-retroarch-react-2/vite.config.ghpages.ts
+++ examples/demo-retroarch-react-2/vite.config.ghpages.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/retroarch-react-demo-site",
   plugins: [react()],
+  base: '/',  // GitHub Pagesの '/retroarch-js/' から変更
 })
