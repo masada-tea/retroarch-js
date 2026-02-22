3c12b8ac83dadd08f3030e5ea0bfdf9ede7d73b7
masada-tea <64900337+masada-tea@users.noreply.github.com>
2026-02-23 03:39:36 +0900

    mkxp-z_libretro.options.js を更新

--- examples/demo-retroarch-react-2/public/cores/mkxp-z_libretro.options.js
+++ examples/demo-retroarch-react-2/public/cores/mkxp-z_libretro.options.js
@@ -1,5 +1,5 @@
 {
-  "corename": "mkxp-z",
+  "corename" "mkxp-z",
   "displayname": "Mkxpz",
   "libname": "mkxp-z_libretro", // 拡張子なしのファイル名
   "supported_extensions": ["mkxpz", "zip", "7z"] // コアが対応する拡張子
